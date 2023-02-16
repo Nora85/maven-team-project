59f38c8d322539cac51be9b5ce784a91bc55eeee
ASG <gulisikaynur@gmail.com>
2023-02-16 12:59:10 -0600

    My first commit

--- src/main/java/aynur/Hello.java
+++ src/main/java/aynur/Hello.java
@@ -3,5 +3,6 @@ package aynur;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello everyone");
+        System.out.println("Have a good one");
     }
 }
